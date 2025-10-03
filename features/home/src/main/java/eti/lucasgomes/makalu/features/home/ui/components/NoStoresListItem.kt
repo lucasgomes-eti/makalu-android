dc94f7973911fa02e1909eb7c23c2e33b09c17f9
Lucas Gomes <lucasgomes.exe@gmail.com>
2025-10-02 20:05:16 -0400

    feat: add strings to home feature

--- features/home/src/main/java/eti/lucasgomes/makalu/features/home/ui/components/NoStoresListItem.kt
+++ features/home/src/main/java/eti/lucasgomes/makalu/features/home/ui/components/NoStoresListItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import eti.lucasgomes.makalu.features.home.R
 
@@ -25,11 +26,11 @@ internal fun LazyItemScope.NoStoresListItem() {
         Column {
             Icon(
                 painterResource(R.drawable.store),
-                contentDescription = "Store image",
+                contentDescription = stringResource(R.string.accessibility_store_image),
                 modifier = Modifier.Companion.size(128.dp),
                 tint = MaterialTheme.colorScheme.primary
             )
-            Text("No stores found")
+            Text(stringResource(R.string.no_stores_found))
         }
     }
 }
--- features/home/src/main/java/eti/lucasgomes/makalu/features/home/ui/components/RequireAuthDialog.kt
+++ features/home/src/main/java/eti/lucasgomes/makalu/features/home/ui/components/RequireAuthDialog.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import eti.lucasgomes.makalu.components.ExpressiveTextButton
 import eti.lucasgomes.makalu.features.home.R
 
@@ -17,19 +18,24 @@ internal fun RequireAuthDialog(
     onConfirmation: () -> Unit,
 ) {
     AlertDialog(
-        icon = { Icon(painterResource(R.drawable.login), "Login Icon") },
-        title = { Text("Login Required") },
-        text = { Text("You must be logged in to order.") },
+        icon = {
+            Icon(
+                painterResource(R.drawable.login),
+                stringResource(R.string.accessibility_login_icon)
+            )
+        },
+        title = { Text(stringResource(R.string.login_required)) },
+        text = { Text(stringResource(R.string.you_must_be_logged_in_to_order)) },
         onDismissRequest = onDismissRequest,
         confirmButton = {
             ExpressiveTextButton(onClick = onConfirmation) {
-                Text("Login")
+                Text(stringResource(R.string.login))
             }
         },
         dismissButton = {
             ExpressiveTextButton(onClick = onDismissRequest) {
                 Text(
-                    "Dismiss",
+                    text = stringResource(R.string.dismiss),
                     color = colorScheme.contentColorFor(AlertDialogDefaults.containerColor)
                 )
             }
