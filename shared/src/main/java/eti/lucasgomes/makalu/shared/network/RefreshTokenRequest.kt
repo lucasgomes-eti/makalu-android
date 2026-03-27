06b990ebcb398dfad1d9597903c16a11d421ff18
Lucas Gomes <lucasgomes.exe@gmail.com>
2026-03-27 13:37:02 -0400

    fix: update casing in RefreshTokenRequest.kt

--- shared/src/main/java/eti/lucasgomes/makalu/shared/network/RefreshTokenRequest.kt
+++ shared/src/main/java/eti/lucasgomes/makalu/shared/network/RefreshTokenRequest.kt
@@ -1,6 +1,10 @@
 package eti.lucasgomes.makalu.shared.network
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RefreshTokenRequest(val refreshToken: String)
+data class RefreshTokenRequest(
+    @SerialName("refresh_token")
+    val refreshToken: String
+)
